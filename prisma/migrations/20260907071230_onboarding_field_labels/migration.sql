4b89cc574c125003c9676a8c51c37fca10071beb
Vijaysanduri <90255293+Vijaysanduri@users.noreply.github.com>
2026-09-11 21:06:19 +0530

    Create migration.sql

---
+++ prisma/migrations/20260907071230_onboarding_field_labels/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "OnboardingFieldLabel" (
+    "fieldKey" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+
+    CONSTRAINT "OnboardingFieldLabel_pkey" PRIMARY KEY ("fieldKey")
+);
